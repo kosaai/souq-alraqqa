bc12198c5037470e8f62dde99af7b318bcd03d2f
abuluai <84326195+kosaai@users.noreply.github.com>
2026-04-04 21:24:12 +0200

    Add files via upload

--- backend/server.py
+++ backend/server.py
@@ -88,4 +88,20 @@ logger = logging.getLogger(__name__)
 async def shutdown_db_client():
     client.close()
 		
+	from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi import Request
+
+# ربط static
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# ربط templates
+templates = Jinja2Templates(directory="templates")
+
+# الصفحة الرئيسية
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})	
+		
 		
