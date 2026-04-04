bc070b3a5261428cc240421c01eb33f041a56c67
abuluai <84326195+kosaai@users.noreply.github.com>
2026-04-04 21:38:50 +0200

    Add files via upload

--- backend/server.py
+++ backend/server.py
@@ -93,15 +93,13 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi import Request
 
-# ربط static
+# ربط ملفات static (CSS, JS, images)
 app.mount("/static", StaticFiles(directory="backend/static"), name="static")
 
-# ربط templates
+# ربط ملفات HTML
 templates = Jinja2Templates(directory="backend/templates")
 
 # الصفحة الرئيسية
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
-		
-		
