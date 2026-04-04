950e2e1d3434349a2c67e9995386f7a26486abab
abuluai <84326195+kosaai@users.noreply.github.com>
2026-04-04 17:47:35 +0200

    Add files via upload

--- backend/server.py
+++ backend/server.py
@@ -14,7 +14,10 @@ from datetime import datetime, timezone
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
-from db import db, client
+# MongoDB connection
+mongo_url = os.environ['MONGO_URL']
+client = AsyncIOMotorClient(mongo_url)
+db = client[os.environ['DB_NAME']]
 
 # Create the main app without a prefix
 app = FastAPI()
